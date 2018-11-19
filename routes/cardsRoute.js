dfddda1928678e7700bbba0b198ef4e852e8b6db
Pushpendu <contactpushpendu@gmail.com>
2018-11-19 22:23:14 +0530

    changes

--- routes/cardsRoute.js
+++ routes/cardsRoute.js
@@ -55,26 +55,26 @@ router.post('/create', (request, response) => {
         else {
 
             console.log("There");
-            // cards.remove({ userId: request.body.userId }).then(function (err, obj) {
-            // if (err) {
-            //     userLoginResponse.error = true;
-            //     userLoginResponse.message = "Can not delete the old card";
-            //     response.status(200).json(userLoginResponse);
-            // } else {
-            let data = new cards(card);
-            data.save((error, result) => {
-                if (error) {
+            cards.remove({ userId: request.body.userId }).then(function (err, obj) {
+                if (err) {
                     userLoginResponse.error = true;
-                    userLoginResponse.message = "Can not link the card";
-                    response.status(500).json(userLoginResponse);
-                } else {
-                    userLoginResponse.error = false;
-                    userLoginResponse.message = "This card has been linked successfully";
+                    userLoginResponse.message = "Can not delete the old card";
                     response.status(200).json(userLoginResponse);
+                } else {
+                    let data = new cards(card);
+                    data.save((error, result) => {
+                        if (error) {
+                            userLoginResponse.error = true;
+                            userLoginResponse.message = "Can not link the card";
+                            response.status(500).json(userLoginResponse);
+                        } else {
+                            userLoginResponse.error = false;
+                            userLoginResponse.message = "This card has been linked successfully";
+                            response.status(200).json(userLoginResponse);
+                        }
+                    });
                 }
-            });
-            // }
-            // })
+            })
         }
     });
 });
